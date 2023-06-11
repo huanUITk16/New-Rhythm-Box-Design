66317c26e2c0f34bed38e7953ece2174e0119de6
Huan <nvhuan2907@gmail.com>
2023-06-11 22:06:19 +0700

    mainpage for artists

---
+++ new design/MainPageForArtists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace new_design
+{
+    public partial class MainPageForArtists : Form
+    {
+        public MainPageForArtists()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPageForArtists_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
